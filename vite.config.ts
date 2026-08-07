31a66bdb7154e3c4a588742471af5066d489703c
Martin Winandy <2338728+pmwmedia@users.noreply.github.com>
2026-08-07 15:49:37 +0200

    Split UI and lib code strictly

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,8 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@lib": fileURLToPath(new URL("./src/lib", import.meta.url)),
+      "@ui": fileURLToPath(new URL("./src/ui", import.meta.url)),
     },
   },
 })
